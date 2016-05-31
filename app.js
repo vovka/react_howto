435871d90465697375bd57465081f0c2d06aeba8
Volodymyr Shcherbyna <sch.icq@gmail.com>
2016-06-01 02:20:09 +0300

    props from html

--- app.js
+++ app.js
@@ -1,51 +1,6 @@
-var MyComponent = React.createClass({
-  getInitialState: function() {
-    return {
-      isOpen: this.props.show
-    };
-  },
-
-  render: function() {
-    var open = React.createElement('div', null, [
-          React.createElement(
-            'div',
-            {
-              key: 'content',
-              className: 'content'
-            },
-            'Hello world'
-          ),
-          React.createElement(
-            'div',
-            {
-              key: 'closeButton',
-              className: 'closeButton'
-            },
-            'Close'
-          )
-        ]),
-
-        closed = React.createElement('div', null,
-          React.createElement(
-            'div',
-            {
-              key: 'openButton',
-              className: 'openButton'
-            },
-            'Open'
-          )
-        );
-
-    if (this.state.isOpen)
-      return open;
-    else
-      return closed;
-  }
-});
-
-
+var target = document.getElementById('popup');
 
 ReactDOM.render(
-  React.createElement(MyComponent, {show: false}),
-  document.getElementById('popup')
+  React.createElement(Popup, {show: !!target.dataset.show}),
+  target
 );
---
+++ components/popup.js
@@ -0,0 +1,44 @@
+var Popup = React.createClass({
+  getInitialState: function() {
+    return {
+      isOpen: this.props.show
+    };
+  },
+
+  render: function() {
+    var open = React.createElement('div', null, [
+          React.createElement(
+            'div',
+            {
+              key: 'content',
+              className: 'content'
+            },
+            'Hello world'
+          ),
+          React.createElement(
+            'div',
+            {
+              key: 'closeButton',
+              className: 'closeButton'
+            },
+            'Close'
+          )
+        ]),
+
+        closed = React.createElement('div', null,
+          React.createElement(
+            'div',
+            {
+              key: 'openButton',
+              className: 'openButton'
+            },
+            'Open'
+          )
+        );
+
+    if (this.state.isOpen)
+      return open;
+    else
+      return closed;
+  }
+});
