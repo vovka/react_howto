80aec7af8e2059d4bc743d2556ed45ae2aa8dec1
Volodymyr Shcherbyna <sch.icq@gmail.com>
2016-06-01 02:03:38 +0300

    styles

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-alert('ok');
+var MyComponent = React.createClass({
+  render: function() {
+    var open = React.createElement('div', null, [
+          React.createElement(
+            'div',
+            {
+              key: 'content',
+              className: 'content'
+            },
+            'Hello world'
+          ),
+          React.createElement(
+            'div',
+            {
+              key: 'closeButton',
+              className: 'closeButton'
+            },
+            'Close'
+          )
+        ]),
+
+        closed = React.createElement('div', null,
+          React.createElement(
+            'div',
+            {
+              key: 'openButton',
+              className: 'openButton'
+            },
+            'Open'
+          )
+        );
+
+    return open;
+  }
+});
+
+
+
+ReactDOM.render(
+  React.createElement(MyComponent),
+  document.getElementById('popup')
+);
