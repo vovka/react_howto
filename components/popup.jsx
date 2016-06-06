ed4a895375c790aade32b77a07f7ab01e353b28c
Volodymyr Shcherbyna <sch.icq@gmail.com>
2016-06-06 20:35:42 +0300

    vanilla js

--- components/popup.jsx
+++ components/popup.jsx
@@ -1,32 +1,41 @@
-import $ from 'jquery';
-
 let getNextState = (currentState) => {
   if (currentState.isOpen) {
-    return {
-      action: 'hide',
+    return { // opened -> closed
       buttonText: 'Open',
-      isOpen: false
+      isOpen: false,
+      buttonClassNameToRemove: 'closeButton',
+      buttonClassNameToAdd: 'openButton',
+      buttonText: 'Open',
+      contentClassNameToRemove: '',
+      contentClassNameToAdd: 'hidden',
     }
-  } else {
+  } else { // closed -> opened
     return {
-      action: 'show',
       buttonText: 'Close',
-      isOpen: true
+      isOpen: true,
+      buttonClassNameToRemove: 'openButton',
+      buttonClassNameToAdd: 'closeButton',
+      buttonText: 'Close',
+      contentClassNameToRemove: 'hidden',
+      contentClassNameToAdd: '',
     }
   }
 }
 
+const BUTTON_CHILD_INDEX = 1,
+      CONTENT_CHILD_INDEX = 0;
+
 export class Popup {
   constructor(props) {
     this.showByDefault = props.show;
-    this.$el = $(props.el);
-    this.$button = this.$el.find('.button');
-    this.$content = this.$el.find('.content');
+    this.el = props.el;
+    this.button = this.el.children[BUTTON_CHILD_INDEX];
+    this.content = this.el.children[CONTENT_CHILD_INDEX];
     this.isOpen = true;
   }
 
   bind() {
-    this.$button.on('click', this._toggle.bind(this));
+    this.button.addEventListener('click', this._toggle.bind(this));
     if (this.showByDefault) {
       this._toggle();
     }
@@ -39,8 +48,10 @@ export class Popup {
   }
 
   _render(state) {
-    this.$button.toggleClass('closeButton openButton');
-    this.$button.html(state.buttonText);
-    this.$content[state.action]();
+    this.button.className = this.button.className.replace(new RegExp('\\s*' + state.buttonClassNameToRemove + '\\s*', 'g'), '');
+    this.button.className = this.button.className.replace(new RegExp('\\s*' + state.buttonClassNameToAdd + '\\s*', 'g'), '') + ' ' + state.buttonClassNameToAdd;
+    this.button.innerHTML = state.buttonText;
+    this.content.className = this.content.className.replace(new RegExp('\\s*' + state.contentClassNameToRemove + '\\s*', 'g'), '');
+    this.content.className = this.content.className.replace(new RegExp('\\s*' + state.contentClassNameToAdd + '\\s*', 'g'), '') + ' ' + state.contentClassNameToAdd;
   }
 }
